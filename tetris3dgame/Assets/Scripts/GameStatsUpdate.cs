fe6bebd9040679ebef4637dc9a519f9f8d359b11
Edmond-Aimé Kabushemeye Nkinzo <swordman503@gmail.com>
2024-03-05 16:43:40 -0500

    feat(3d game): make game stats dynamic

---
+++ tetris3dgame/Assets/Scripts/GameStatsUpdate.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameStatsUpdate : MonoBehaviour
+{
+    public TMP_Text textMesh;
+    private int level = 1;
+    private int floorsDestroyed = 0;
+    private int score = 0;
+    private int highScore = 0;
+    private string nextTetromino = "3D Hammer";
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textMesh.text = $@"Level: {level}
+Score/HighScore: {score}/{highScore}
+Floors cleared in this level: {floorsDestroyed}
+Next Tetromino: {nextTetromino}";
+    }
+}
